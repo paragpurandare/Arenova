591199d55e27616dfa4ce3cd30abf2bb1fcb410b
paragpurandare <paragpurandare50@gmail.com>
2026-06-19 19:42:04 +0530

    Initial project setup

---
+++ src/main/java/com/arenova/ArenovaSportsEcosystemApplication.java
@@ -0,0 +1,13 @@
+package com.arenova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenovaSportsEcosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenovaSportsEcosystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arenova/ArenovaSportsEcosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arenova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenovaSportsEcosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
